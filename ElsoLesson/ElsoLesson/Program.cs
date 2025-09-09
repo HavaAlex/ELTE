8551069973c69e4defc5d1eedf6e00f7c1a95fc2
Havasi Alex Márton <fxemow@INF.ELTE.HU>
2025-09-09 15:02:38 +0200

    elso prog ora

---
+++ ElsoLesson/ElsoLesson/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElsoLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+            Random random = new Random();
+            while (true)
+            {
+                for (int k = 0; k < 15; k++)
+                {
+                    int i = random.Next(0, colors.Length);
+                    Console.BackgroundColor = colors[i];
+                    Console.ForegroundColor = colors[colors.Length - i - 1];
+                    string[] Words = { "Anyádat", "Faszom", "Picsába", "Seggem", "Baszdmeg" };
+                    int j = random.Next(0, Words.Length);
+                    Console.Write(Words[j]);
+                }
+                
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Téglalap terület számolás/Téglalap terület számolás/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Téglalap_terület_számolás
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string error = "";
+            AskQuestion(error);
+            Console.ReadKey();
+        }
+        static void AskQuestion(string error)
+        {
+            Console.WriteLine(error);
+            Console.Write("Adja meg az egyik oldalt (egész szám legyen légyszi és ne 0 és ne negatív pliz pliz): ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Adja meg az másik oldalt (egész szám legyen légyszi és ne 0 és ne negatív pliz pliz): ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            if (a == 0 || b == 0)
+            {
+                Console.Clear();
+                AskQuestion("NE LEGYEN NULLA NAAAA");
+            }
+            else if (a < 0 || b < 0) 
+            {
+                Console.Clear();
+                AskQuestion("Legyen már bozítív naaa");
+            }
+            else 
+            {
+                Console.WriteLine(a*b + " lett az eredmény");
+                Console.Write("Akarod újra? (y/n)");
+                string answer = Console.ReadLine();
+                if (answer == "y" || answer == "yes") 
+                {
+                    error = "";
+                    Console.Clear();
+                    AskQuestion(error);
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Mindegy amúgysem akartam, lépj ki nyugodtan");
+                }
+
+                //double tryParse(Console.Readline(), out a);
+
+            }
+        }
+    }
+}
