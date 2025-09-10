7c5369f5879246ed15692d9cd155ea3aa4f4d62b
Havasi Alex Márton <fxemow@INF.ELTE.HU>
2025-09-10 13:33:15 +0200

    elso haskell ora tök jó volt köszi

---
+++ 1.félév/Funkcionális programozás/test.hs
@@ -0,0 +1,28 @@
+module Test where
+x :: Int 
+x = 3 
+
+
+y :: Integer --ez a long (vicces)
+y = 4
+
+{- nagyon szép komment-}
+-- nem lehet felüldefiniálni, viszont mindenre hivatkozhatsz mindenhol
+ezjo :: Integer
+ezjo = 3 + (-4)
+
+
+
+--foo :: Int -> Int -> Char --kettő intet vár és egy karakterrel tér vissza
+
+
+inc :: Int -> Int
+--inc = undefined ilyenkor nem lesz belölle baj
+inc x = x + 1 --baloldal a lesza  neve a paramnak. több param esetén többet irsz oda
+
+
+double :: Int -> Int
+double x = x *2
+
+sumold :: Int -> Int -> Int
+sumold x y = x + y 
