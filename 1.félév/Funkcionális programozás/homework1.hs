938bb6910d134fae58a97b9f0df4c6292cb99947
HavaAlex <havasi.alex.marton@gmail.com>
2025-09-17 09:00:19 +0200

    1. Haskell házi kész

---
+++ 1.félév/Funkcionális programozás/homework1.hs
@@ -0,0 +1,22 @@
+module HomeWork1 where
+
+--1
+myFavouriteOneDigitNumber :: Int
+myFavouriteOneDigitNumber = 8
+--2
+myFavouriteOneDigitNumbersEvilTwin  :: Int
+myFavouriteOneDigitNumbersEvilTwin = myFavouriteOneDigitNumber * 2 -13
+--3
+myFavouriteOneDigitNumbersAndTheirEvilTwinsDistantRelative :: Int -> Int
+myFavouriteOneDigitNumbersAndTheirEvilTwinsDistantRelative x = myFavouriteOneDigitNumber * x + myFavouriteOneDigitNumbersEvilTwin * x * 3 - 2
+--4 
+isTheLastNumberTooLarge :: Int -> Int -> Int -> Bool
+isTheLastNumberTooLarge x y z = 
+    if z > x+y+100 then True
+    else False
+--5
+isTheEvilDistantRelativeTooLargeForMyFavouriteOneDigitNumberAndItsEvilTwin :: Int -> Bool
+isTheEvilDistantRelativeTooLargeForMyFavouriteOneDigitNumberAndItsEvilTwin x =
+    if myFavouriteOneDigitNumbersAndTheirEvilTwinsDistantRelative x >myFavouriteOneDigitNumber+myFavouriteOneDigitNumbersEvilTwin+100 then True
+    else False 
+    
